9260dd307219456ce2283323fa1c0922f5946ef0
Alexey Shevtsov <a.shevtsov@itransition.com>
2023-02-06 11:41:33 +0100

    feat: add message on adding to favorites

--- src/db/mock-data/albums.mock.ts
+++ src/db/mock-data/albums.mock.ts
@@ -12,7 +12,7 @@ export default () => [
     artistId: '3342fbea-8ebc-4033-9f59-afe8977aeefa',
   },
   {
-    id: '08cd97f2-13d9-4d81-a8d9-968662070dr1',
+    id: '64f16f3f-4923-42b7-8749-e0991b342dad',
     name: 'KC Album 1',
     year: 2002,
     artistId: 'ffc57ebb-6a9e-453d-9a53-160aef4a7400',
--- src/domains/favorites/favorites.controller.ts
+++ src/domains/favorites/favorites.controller.ts
@@ -18,6 +18,7 @@ import { NotInFavoritesError } from './errors/not-in-favorites.error';
 import { UnprocessableEntityException } from '@nestjs/common/exceptions/unprocessable-entity.exception';
 import { ArtistsService } from '../artists/artists.service';
 import { AlbumsService } from '../albums/albums.service';
+import { OutputAddedDto } from './dto/output-added.dto';
 
 @Controller('favs')
 export class FavoritesController {
@@ -39,48 +40,48 @@ export class FavoritesController {
   async addTrack(
     @Param('id', new ParseUUIDPipe({ version: DEFAULT_UUID_VERSION_NUMBER }))
     id: string,
-  ) {
+  ): Promise<OutputAddedDto> {
     const track = await this.trackService.findOne(id);
 
     if (!track) {
       throw new UnprocessableEntityException(errorMessages.TRACK_NOT_FOUND);
     }
 
-    const fav = await this.favoritesService.addTrack(track.id);
+    await this.favoritesService.addTrack(track.id);
 
-    return fav;
+    return { added: true, message: `Track ${track.id} added to favorites` };
   }
 
   @Post('artist/:id')
   async addArtist(
     @Param('id', new ParseUUIDPipe({ version: DEFAULT_UUID_VERSION_NUMBER }))
     id: string,
-  ) {
+  ): Promise<OutputAddedDto> {
     const artist = await this.artistService.findOne(id);
 
     if (!artist) {
       throw new UnprocessableEntityException(errorMessages.ARTIST_NOT_FOUND);
     }
 
-    const fav = await this.favoritesService.addArtist(artist.id);
+    await this.favoritesService.addArtist(artist.id);
 
-    return fav;
+    return { added: true, message: `Artist ${artist.id} added to favorites` };
   }
 
   @Post('album/:id')
   async addAlbum(
     @Param('id', new ParseUUIDPipe({ version: DEFAULT_UUID_VERSION_NUMBER }))
     id: string,
-  ) {
+  ): Promise<OutputAddedDto> {
     const album = await this.albumService.findOne(id);
 
     if (!album) {
       throw new UnprocessableEntityException(errorMessages.ALBUM_NOT_FOUND);
     }
 
-    const fav = await this.favoritesService.addAlbum(album.id);
+    await this.favoritesService.addAlbum(album.id);
 
-    return fav;
+    return { added: true, message: `Album ${album.id} added to favorites` };
   }
 
   @Delete('track/:id')
